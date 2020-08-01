ea8671807099da9ba17484742f991d5f89c86c7a
Nurbiye <nurbiye720@gmail.com>
2020-08-01 15:15:45 -0500

    update

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,5 +1,14 @@
 public class BookARoom {
     public static void main(String[] args) {
         System.out.println("We moved a new house, i don't want to book a room");
+
+
+        /*  to push the code to github
+        1.write a code
+        2.add it to staging area  {git add .}
+        3.commit it(screen shot), {git commit -m"" }
+        4.push it to github: {git push}
+        5.then go to the github website to refresh the page
+         */
     }
 }
