a4a20f48436814c147d74a50457ef9a72a7590c7
Nurbiye <nurbiye720@gmail.com>
2020-08-01 11:09:30 -0500

    my feeling

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -10,8 +10,11 @@ public class HappyB20 {
         step3:
         git commit -m"add HappyB20.java"     //take snapshot(save in git history)
                     // update instead of add if we updated the previous code
+                   //-m"this is newest commit"
+                   //(write something meaningful message)
 
          */
         System.out.println("we moved yesterday, it was Eid day, but an extremely tired day ");
+        System.out.println("Today August1, I am having git class and still have to facilitate the new house ");
     }
 }
