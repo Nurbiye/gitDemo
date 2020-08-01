9d85728f7316d4f24b8380518d54297afa5c48e3
Nurbiye <nurbiye720@gmail.com>
2020-08-01 10:53:22 -0500

    update HelloB20

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,16 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is happy batch");
+
+        /*  code--add--commit
+        step1:
+        git init      //to know if we have repository
+        step2:
+        git add .      //add the code in staging area
+        step3:
+        git commit -m"add HappyB20.java"     //take snapshot(save in git history)
+                    // update instead of add if we updated the previous code
+
+         */
     }
 }
--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,6 +2,8 @@ public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello B20, NOW WE START GIT CLASS!");
 
+        System.out.println("this first day git class is on July29,2020");
+
 
 
 
