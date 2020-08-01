d698778c33321f46bdca4201abe70789059b68ef
Nurbiye <68974712+Nurbiye@users.noreply.github.com>
2020-08-01 15:14:39 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -2,6 +2,13 @@ public class BookARoom {
     public static void main(String[] args) {
         System.out.println("We moved a new house, i don't want to book a room");
         
-        //i added this line from github 
+        /* i write these in github and git pull in intelij
+        1.open github in webside
+        2.your repository --> gitDemo-->src-->open the class i want to see
+        3.press edit-->wrote my update on code--->scroll down --> commit changes
+        4.go to intelij, on terminal in the bottom---> git pull
+        5. then i can see the codes that i changed in github
+        */
+ 
     }
 }
